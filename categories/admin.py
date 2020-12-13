d4a0e773caa248843f113e0e21dc1d21299b18ee
Ahmed Samir <a.samir9710@gmail.com>
2020-12-13 13:41:00 +0200

    added categories and sub categories to the product

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category, SubCategory
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    sub_categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='sub_categories')
 
     def __str__(self):
         return self.name
