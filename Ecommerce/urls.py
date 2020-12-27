99f3d1484c463c44a49f9ca4b38823c0371ee3dc
Ahmed Samir <a.samir9710@gmail.com>
2020-12-27 17:06:16 +0200

    fixes get images issue

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -42,8 +44,9 @@ urlpatterns = [
     path('api-categories/', include('categories.urls', namespace='categories')),
     path('api-cart/', include('cart.urls', namespace='cart')),
     path('api-cities/',include('cities.urls', namespace='cities')),
-    
+
     # swagger documentation
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
