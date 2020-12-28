77296f44b03700b8a8abd6033f92035a67fef934
Ahmed Samir <a.samir9710@gmail.com>
2020-12-28 12:04:06 +0200

    added translation for categories and subcategories

--- categories/models.py
+++ categories/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
-
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name_en = models.CharField(max_length=50)
+    name_ar = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name_en
 
 
 class SubCategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='all_subcategories', null=True)
-    name = models.CharField(max_length=50)
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='all_subcategories')
+    name_en = models.CharField(max_length=50)
+    name_ar = models.CharField(max_length=50, null=True, blank=True)
+
     def __str__(self):
-        return self.name
+        return self.name_en
