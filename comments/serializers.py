afcd88581436ab40c943dd8a5b3f0bc37890cd3a
Ahmed Samir <a.samir9710@gmail.com>
2020-12-30 23:43:52 +0200

    added user serializer in comment serializer list

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Comment
+from users.serializers import UserDataSerializer
 
 class CommentDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +9,7 @@ class CommentDetailSerializer(serializers.ModelSerializer):
         fields = ('content',)
         
 class CommentSerializer(serializers.ModelSerializer):
+    author = UserDataSerializer()
     class Meta:
         model = Comment
         fields = '__all__'
