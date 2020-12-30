3978ad88421686e5c0060595f423fed9808dd7a1
Ahmed Samir <a.samir9710@gmail.com>
2020-12-31 00:46:07 +0200

    added user serializer for owner in cart products

--- cart/serializers.py
+++ cart/serializers.py
@@ -5,13 +5,15 @@ from products.models import Product
 from cities.serializers import CitySerializer
 from categories.serializers import CategorySerializer, SubCategorySerializer
 from products.serializers import ProductImageSerializer
+from users.serializers import UserDataSerializer
 
 class ProductItemSerializers(serializers.ModelSerializer):
     city = CitySerializer()
     category = CategorySerializer()
     sub_category = SubCategorySerializer()
     images = serializers.SerializerMethodField()
-
+    owner = UserDataSerializer()
+    
     def get_images(self, obj):
         serializer = ProductImageSerializer(obj.images.all(), many=True)
         return serializer.data
