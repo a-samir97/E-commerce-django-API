b753fd4be7cb11b6254cee24cdf0155f0797485b
Ahmed Samir <a.samir9710@gmail.com>
2021-01-16 00:04:32 +0200

    added caegories prices

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import Category, SubCategory
-
+     
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
